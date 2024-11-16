9788cc6b0f8af64e85345385b63c74b620422cf4
Andre-Castilho <kastilho.andre@gmail.com>
2024-11-16 03:01:03 -0300

    Versão com classes

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import threading
-import random 
 import time
+import random
 import sys
 
-pontos_redistribuicao = 0
-veiculos = 0
-encomendas = 0
-espacos_de_carga = 0 #por veiculo
+# Coloquei uns nomes que fazem mais ou menos sentido com a abreviação, se quiser mudar fique a vontade
 
-semafore = threading.Semaphore(1)
+# S = nº de Stations
+# C = nº de Carriers
+# P = nº de Packages
+# A = Carrier.capacity (amount ficaria esquisitasso e nem faria sentido)
+
+class Package(threading.Thread):
+    def __init__(self, package_id, redist_stations):
+        threading.Thread.__init__(self)
+        self.package_id = package_id
+        order = random.sample(redist_stations, 2)
+        self.origin = order[0]
+        self.destination = order[1]
+        self.redist_stations = redist_stations
+
+class Carrier(threading.Thread):
+    def __init__(self, carrier_id, redist_stations, capacity):
+        threading.Thread.__init__(self)
+        self.carrier_id = carrier_id
+        self.redist_stations = redist_stations
+        self.capacity = capacity
+        self.packages = []
+        self.current_station = random.choice(redist_stations)
+
+class Station(threading.Thread):
+    def __init__(self, station_id):
+        threading.Thread.__init__(self)
+        self.station_id = station_id
+        self.packages = []
+        self.carriers = []
 
 if len(sys.argv) != 5:
     print("Usage: python Untitled-1.py <S> <C> <P> <A>")
@@ -19,49 +44,36 @@ C = int(sys.argv[2])
 P = int(sys.argv[3])
 A = int(sys.argv[4])
 
-
-
 if P <= A or P <= C or A <= C:
-    print("Encomendas insuficientes para distribuir")
+    print("Insufficient packages to distribute")
     exit()
 
+def main():
+    stations = []
+    carriers = []
+    packages = []
 
-def criar_threads(P, C, S, A):
-     pontos_redistribuicao = []
-    veiculos = []
-    encomendas = []
-    espacos_de_carga = A
-
-    # Criar threads para cada encomenda.
-    for i in range(P):
-        encomendas = threading.Thread(target=, args=(i,))
-        threads.append(encomendas)
-        thread.start()
-
-    # Criar threads para cada veículo.
-    for i in range(C):
-        ponto_partida = random.choice(pontos_redistribuicao)
-        veiculos = threading.Thread(target= processamento_veiculos, args=(i, ponto_partida))
-        threads.append(veiculos)
-        thread.start()
-
-    # Criar threads para cada ponto de redistribuição.
     for i in range(S):
-        pontos_redistribuicao = threading.Thread(target= , args=(i, espacos_de_carga))
-        threads.append(pontos_redistribuicao)
-        thread.start()
-
-    # Esperar todas as threads terminarem.
-    for thread in threads:
-        thread.join()
-
-
-def processamento_veiculos(i, ponto_partida):
-    print(f"Veículo {i} partindo do ponto de redistribuição {ponto_partida}.")
-
-    # Simular tempo de viagem.
+        station = Station(i)
+        stations.append(station)
+        station.start()
     
+    for i in range(C):
+        carrier = Carrier(i, stations, A)
+        carriers.append(carrier)
+        carrier.start()
+    
+    for i in range(P):
+        package = Package(i, stations)
+        packages.append(package)
+        package.start()
 
+    """ 
+        package não tá em join pq pelo q entendi a thread encerra quando a entrega é feita, 
+        mas tbm não sei se dá pra fazer isso em um método da própria classe
+    """
+    for carrier in carriers:
+        carrier.join()
 
-# Exemplo de uso:
-criar_threads(P=5, C=3, S=2)
+    for station in stations:
+        station.join()
